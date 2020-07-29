0a60a2f0f9442cee31ed26ba125ee6d897a3685a
Burak Donat <burakdonat@gmail.com>
2020-07-29 11:18:06 +0300

    Create skeleton project

---
+++ LSCase-Burak/Conrollers/FavouritesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavouritesViewController.swift
+//  LSCase-Burak
+//
+//  Created by Burak Donat on 29.07.2020.
+//  Copyright © 2020 Burak Donat. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesViewController: UIViewController {
+
+    @IBOutlet weak var favouritesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ LSCase-Burak/Conrollers/GamesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GamesViewController.swift
+//  LSCase-Burak
+//
+//  Created by Burak Donat on 29.07.2020.
+//  Copyright © 2020 Burak Donat. All rights reserved.
+//
+
+import UIKit
+
+class GamesViewController: UIViewController {
+
+    @IBOutlet weak var gamesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
